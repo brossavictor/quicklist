a84730e54c6675f2f37d94203c6020bf79acda8e
Victor Brossa <brossajoao@gmail.com>
2025-02-03 18:02:48 -0500

    done

--- scripts/deleteItem.js
+++ scripts/deleteItem.js
@@ -1,11 +1,13 @@
 export function deleteItem() {
 	const buttons = document.querySelectorAll(".delete");
+	const warning = document.getElementById("warning");
 
 	buttons.forEach((button) => {
 		button.addEventListener("click", function () {
 			let item = this.closest(".item-card");
 			if (item) {
 				item.remove();
+				warning.style.visibility = "visible";
 			}
 		});
 	});
